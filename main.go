6d9cacca9cb6af82f40ed024c4931f2309f63b07
Joao Paulo Dubas <joao.dubas@gmail.com>
2015-05-18 13:17:51 -0300

    [update] Separate statements into own func.

--- main.go
+++ main.go
@@ -11,13 +11,32 @@ func main() {
 	handleError("Error connecting with redis:", err)
 	defer conn.Close()
 
+	setString(conn)
+	setSet(conn)
+	setPubSub(conn)
+	setMultiExec(conn)
+	setMultiDiscard(conn)
+}
+
+func handleError(msg string, err error) {
+	if err != nil {
+		log.Fatal(msg, err)
+		os.Exit(1)
+	}
+}
+
+// toy method ------------------------------------------------------------------
+
+func setString(conn *Client) {
 	conn.Set("key:string", "ola")
 	r, err := conn.Get("key:string")
 	handleError("Error getting key:", err)
 	fmt.Printf("the key %s is %s\n", "key:string", r)
+}
 
+func setSet(conn *Client) {
 	conn.Sadd("key:set", "ola", "mundo")
-	r, err = conn.Scard("key:set")
+	r, err := conn.Scard("key:set")
 	handleError("Error getting key:", err)
 	fmt.Printf("the key %s has len %s\n", "key:set", r)
 	r, err = conn.Smembers("key:set")
@@ -25,14 +44,16 @@ func main() {
 	for _, e := range r.Elems {
 		fmt.Printf("the key %s has elem %s\n", "key:set", e)
 	}
+}
 
+func setPubSub(conn *Client) {
 	nconn, err := NewClient("127.0.0.1:6379")
 	handleError("Error getting key:", err)
 	psconn := NewPSClient(nconn)
 	defer psconn.Close()
 
 	sub := psconn.Subscribe("channel:test")
-	r, err = conn.Publish("channel:test", "hello")
+	r, err := conn.Publish("channel:test", "hello")
 	handleError("Did not publish message:", err)
 	sub = psconn.Receive()
 	if !sub.Timeout() {
@@ -40,7 +61,9 @@ func main() {
 	} else {
 		handleError("Timeout for receive message", fmt.Errorf("Timeout"))
 	}
+}
 
+func setMultiExec(conn *Client) {
 	conn.Multi()
 	conn.Add("set", "key:pipe:1", "ola")
 	conn.Add("set", "key:pipe:2", "hello")
@@ -50,26 +73,23 @@ func main() {
 	ar2 := conn.Add("get", "key:pipe:2")
 	ar3 := conn.Add("get", "key:pipe:3")
 	ar4 := conn.Add("get", "key:pipe:4")
-	r, err = conn.Exec()
+	r, err := conn.Exec()
 	handleError("Something went wrong with pipe:", err)
 	fmt.Printf("Responses: %s, %s, %s, %s\n", <-ar1, <-ar2, <-ar3, <-ar4)
 	for i, e := range r.Elems {
 		fmt.Printf("Result for op %d: %s\n", i, e)
 	}
+}
 
+func setMultiDiscard(conn *Client) {
 	conn.Multi()
 	conn.Add("set", "key:pipe:1", "ola")
 	conn.Add("set", "key:pipe:2", "hello")
 	conn.Add("get", "key:pipe:1")
 	conn.Add("get", "key:pipe:2")
-	r, err = conn.Discard()
+	r, err := conn.Discard()
 	handleError("Something went wrong with pipe:", err)
 	fmt.Printf("Result for discard %s", r)
 }
 
-func handleError(msg string, err error) {
-	if err != nil {
-		log.Fatal(msg, err)
-		os.Exit(1)
-	}
 }
