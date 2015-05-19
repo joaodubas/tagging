81884504bc999f878881e09e2bcba6ffa1180c7c
Joao Paulo Dubas <joao.dubas@gmail.com>
2015-05-19 00:40:32 -0300

    [update] auto update tag when redis signals.
    The auto update is started when the routine is initialized and runs
    everytime redis sends a message on the keyspace notification channel. This
    way, anyone changing a given tag property in redis will also trigger a
    change in go tag state.

--- tag.go
+++ tag.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/fzzy/radix/redis"
 	"log"
 	"reflect"
+	"strings"
 	"time"
 )
 
@@ -103,7 +105,11 @@ func (v *Vector) Append(tag Tagger) {
 	if err != nil {
 		log.Printf("Could not append %s into vector %s\n", tag, v.Name)
 	} else {
-		v.Tags = append(v.Tags, n)
+		if d, ok := n.(string); ok {
+			v.Tags = append(v.Tags, d)
+		} else {
+			log.Printf("Tag %s has improper name %s", tag, n)
+		}
 	}
 }
 
@@ -123,6 +129,7 @@ func NewVector(name string, conn *Client, args ...string) *Vector {
 
 type Tag struct {
 	conn        *Client
+	psconn      *PSClient
 	Name        string
 	Description string
 	Value       int
@@ -149,6 +156,9 @@ func (t *Tag) Init() error {
 	t.conn.Add("set", t.key(t.Name, "quality"), t.Quality)
 	t.conn.Add("set", t.key(t.Name, "timestamp"), t.Timestamp)
 	_, err := t.conn.Exec()
+	if err == nil {
+		t.auto()
+	}
 	return err
 }
 
@@ -157,8 +167,10 @@ func (t *Tag) Get(tag string, prop string) (interface{}, error) {
 }
 
 func (t *Tag) Set(tag string, prop string, args ...interface{}) error {
-	_, err := t.conn.Set(t.key(tag, prop), args)
-	return err
+	if prop == "Timestamp" || prop == "Name" {
+		return fmt.Errorf("%s property is not user editable.", prop)
+	}
+	return t.update(tag, prop, args)
 }
 
 func (t *Tag) key(tag string, prop string) string {
@@ -169,27 +181,75 @@ func (t *Tag) update(tag string, prop string, args ...interface{}) error {
 	now := ts()
 
 	t.conn.Multi()
-	t.conn.Add("set", t.key(tag, prop), args)
-	t.conn.Add("set", t.key(tag, "Timestamp"), now)
+	t.conn.Add("set", t.key(tag, strings.ToLower(prop)), args)
+	t.conn.Add("set", t.key(tag, "timestamp"), now)
 	_, err := t.conn.Exec()
 
 	if err != nil {
 		return err
 	}
 
-	err = concreteSetProp(t, prop, args)
-
-	if err != nil {
-		return err
-	}
-
-	t.Timestamp = now
 	return nil
 }
 
-func NewTag(conn *Client, name string, description string, value int, quality int) *Tag {
+func (t *Tag) auto() {
+	go func() {
+		converter := func(prop string, v *redis.Reply) (interface{}, error) {
+			switch prop {
+			case "Value", "Quality":
+				return v.Int()
+			case "Timestamp":
+				return v.Int64()
+			case "Name", "Description":
+				return v.Str()
+			}
+			return nil, fmt.Errorf("Missing case for prop %s\n", prop)
+		}
+
+		wait := 500 * time.Millisecond
+
+		errHandler := func(msg string, args ...interface{}) {
+			log.Printf(msg, args...)
+			time.Sleep(wait)
+		}
+
+		t.psconn.PSubscribe(fmt.Sprintf("__keyspace@0__:%s", t.key(t.Name, "*")))
+		for {
+			sub := t.psconn.Receive()
+			if sub.Timeout() {
+				errHandler("Timedout when receiving update for %s.\n", t.Name)
+				continue
+			}
+
+			s := strings.Split(strings.SplitN(sub.Channel, ":", 2)[1], ":")
+			k, p := strings.Join(s[:2], ":"), strings.Title(s[2])
+			v, err := t.conn.Get(strings.Join(s, ":"))
+			if err != nil {
+				errHandler("Couldn't get value for key:\n", strings.Join(s, ":"))
+				continue
+			}
+
+			cv, err := converter(p, v)
+			if err != nil {
+				errHandler("Error during convertion: %s\n", err)
+				continue
+			}
+
+			err = concreteSetProp(t, p, cv)
+			if err != nil {
+				errHandler("Couldn't set property %s to value %s in %s\n", p, v.String(), k)
+				continue
+			}
+
+			time.Sleep(wait)
+		}
+	}()
+}
+
+func NewTag(conn *Client, psconn *PSClient, name string, description string, value int, quality int) *Tag {
 	t := &Tag{
 		conn:        conn,
+		psconn:      psconn,
 		Name:        name,
 		Description: description,
 		Value:       value,
